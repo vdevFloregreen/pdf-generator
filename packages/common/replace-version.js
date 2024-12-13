4f0987ddbeb5135e8ff01d9cab2f592dd31e7ab7
vdevFloregreen <vinniciussantana.vs@gmail.com>
2024-12-13 19:04:35 -0300

    Adaptation2

--- packages/common/replace-version.js
+++ packages/common/replace-version.js
@@ -3,12 +3,15 @@ const path = require('path');
 const { execSync } = require('child_process');
 
 try {
-  const gitTag = execSync('git describe --tags $(git rev-list --tags --max-count=1)', { encoding: 'utf8' }).trim();
+  // Get the latest tag using git rev-list and git describe
+  const latestTag = execSync('git rev-list --tags --max-count=1', { encoding: 'utf8' }).trim();
+  const gitTag = execSync(`git describe --tags ${latestTag}`, { encoding: 'utf8' }).trim();
 
   const filePath = path.join(__dirname, 'src/constants.ts');
 
   let content = fs.readFileSync(filePath, 'utf8');
 
+  // Replace the PDFME_VERSION constant with the latest git tag
   content = content.replace(/export const PDFME_VERSION = '.*';/, `export const PDFME_VERSION = '${gitTag}';`);
 
   fs.writeFileSync(filePath, content, 'utf8');
